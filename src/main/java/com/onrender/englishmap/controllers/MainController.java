4db06128d0c2211872c564e4e1066f295b46aabd
EugeneZakhno <zohan1234567@gmail.com>
2023-11-01 16:50:59 +0300

    added home.html

--- src/main/java/com/onrender/englishmap/controllers/MainController.java
+++ src/main/java/com/onrender/englishmap/controllers/MainController.java
@@ -2,11 +2,12 @@ package com.onrender.englishmap.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-    public String home (Model model){
+    @GetMapping("/")    public String home (Model model){
         model.addAttribute("title", "Main page");
         return "home";
     }
